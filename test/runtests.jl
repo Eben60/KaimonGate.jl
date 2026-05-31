682e4dde73213292efe623db4ddb86811b01d171
Ben Elkin <8xe8n-cm@online.de>
2026-05-31 13:42:13 +0200

    test suite created

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,8 @@
-using KaimonGate
-using Test
-using Aqua
+using SafeTestsets
 
-@testset "KaimonGate.jl" begin
-    @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(KaimonGate)
-    end
-    # Write your tests here.
-end
+@safetestset "Aqua" include("src/test_aqua.jl")
+@safetestset "Type metadata" include("src/test_type_meta.jl")
+@safetestset "Value coercion" include("src/test_coercion.jl")
+@safetestset "Tool dispatch" include("src/test_dispatch.jl")
+@safetestset "Message handler" include("src/test_handle_message.jl")
+@safetestset "ZMQ integration" include("src/test_integration.jl")
